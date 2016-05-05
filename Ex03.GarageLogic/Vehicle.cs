684225eadb77494884283809f9f2dd989cbc50f9
David Rubin <dudrubin@gmail.com>
2016-05-05 21:02:39 +0300

    Added vehicle and wheel classes , also added skeltons for electric and petrol vehicles

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class Vehicle
+    {
+        private string m_modelName;
+        private string m_licenseNumber;
+        private float m_sourceEnergyLeft;
+        private List<Wheel> m_wheels;
+
+        public Vehicle()
+        {
+            
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Wheel
+    {
+        private string m_manufacturerName;
+        private float m_currentAirPressure;
+        private float m_maxAirPressure;
+
+        public Wheel()
+        {
+            
+        }
+
+        public void Inflate(float i_airAddition)
+        {
+            //TODO: check max exception
+            m_currentAirPressure += i_airAddition;
+        }
+    }
+}

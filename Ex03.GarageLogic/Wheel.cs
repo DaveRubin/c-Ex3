aae9a767a127605b1c707712d944cfe383cb4eaf
David Rubin <dudrubin@gmail.com>
2016-05-06 17:20:57 +0300

    Updated Wheel class

--- Ex03.GarageLogic/Wheel.cs
+++ Ex03.GarageLogic/Wheel.cs
@@ -6,18 +6,31 @@ namespace Ex03.GarageLogic
 {
     class Wheel
     {
-        private string m_manufacturerName;
+        public readonly string r_manufacturerName;
+        public readonly float r_maxAirPressure;
         private float m_currentAirPressure;
-        private float m_maxAirPressure;
 
-        public Wheel()
+        public float CurrentAirPressure 
         {
-            
+            get
+            {
+                return m_currentAirPressure;
+            }
+        }
+
+        public Wheel(string i_manufacturerName,float i_maxAirPressure)
+        {
+            r_manufacturerName = i_manufacturerName;
+            r_maxAirPressure = i_maxAirPressure;
+            m_currentAirPressure = i_maxAirPressure;
         }
 
         public void Inflate(float i_airAddition)
         {
-            //TODO: check max exception
+            if (m_currentAirPressure + i_airAddition > r_maxAirPressure)
+            {
+                //TODO: check max exception
+            }
             m_currentAirPressure += i_airAddition;
         }
     }

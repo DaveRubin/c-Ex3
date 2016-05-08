7d11dced8f4bd365d9e4bb343d174c32338ba743
barvazos <amitaihandler@gmail.com>
2016-05-08 14:28:58 +0300

    Created owner and garage record
    for holding data in a structured manner

---
+++ Ex03.GarageLogic/VehicleOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class VehicleOwner
+    {
+        public readonly string r_Name;
+        public readonly string r_PhoneNumber;
+
+        internal VehicleOwner(string i_Name, string i_PhoneNumber)
+        {
+            r_Name = i_Name;
+            r_PhoneNumber = i_PhoneNumber;
+        }
+    }
+}

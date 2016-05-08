8d2d2bab43f90eaba69563c235cc42979c585afb
barvazos <amitaihandler@gmail.com>
2016-05-08 14:34:08 +0300

    changed data members according to Guy's style guide
    data members begin with capital letter after underscore (local variables
    with lower case)

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -6,22 +6,37 @@ namespace Ex03.GarageLogic
 {
     internal class Vehicle
     {
-        private PowerSource m_powerSource;
-        private string m_modelName;
-        private string m_licenseNumber;
-        private List<Wheel> m_wheels;
+        private PowerSource m_PowerSource;
+        private string m_ModelName;
+        private string m_LicensePlateNumber;
+        internal List<Tire> m_Tires;
 
         public float GetSourceEnergyPercentage
         {
             get
             {
-                return m_powerSource.GetPercentageLeft;
+                return m_PowerSource.GetPercentageLeft;
+            }
+        }
+
+        public string GetLicensePlateNumber
+        {
+            get
+            {
+                return m_LicensePlateNumber;
             }
         }
 
         public Vehicle()
         {
             
+        }
+
+        public override string ToString()
+        {
+            /// TODO: implement override that iterates through all of the data members and prints them out
+            /// as Guy shows in the overriding object methods video
+            return string.Format("...");
         }
     }
 }
